7af99c9081453bf0362edb1525032a59b5eace7a
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-10-09 08:08:23 -0400

    Challenge completed

--- Models/Door.cs
+++ Models/Door.cs
@@ -3,47 +3,89 @@ namespace _027_bb_the_locked_door.Models;
 public class Door
 {
     // Properties
-    public bool Open { get; set; } = false;
-    public bool Closed { get; set; } = false;
-    public bool Unlocked { get; set; } = false;
-    public bool Locked { get; set; } = false;
+    public bool Open { get; set; }
+    public bool Closed { get; set; }
+    public bool Unlocked { get; set; }
+    public bool Locked { get; set; }
     public string Passcode { get; set; }
     
     // Constructors
     public Door()
     {
         Passcode = InitialPasscode(); // Set starting password
-        Closed = true; // Set door state
+        Closed = true;
+        Locked = true;
+
     }
 
     // Door Methods
     private string InitialPasscode()
     {
-        Console.WriteLine("*** Door Creator 3000 ***");
-        Console.WriteLine("Please enter a 4 digit numerical passcode for your door");
+        bool isValidPasscode = false;
+        while (!isValidPasscode)
+        {
+            Console.WriteLine("*** Door Creator 3000 ***");
+            Console.WriteLine("Please enter a 4 digit numerical passcode for your door");
 
+            string? passcode = Console.ReadLine();
+
+            // The happy path check
+            bool blankResponse = String.IsNullOrEmpty(passcode);
+            if (!blankResponse)
+            {
+                if (passcode!.Length == 4)
+                {
+                    if (int.TryParse(passcode, out int result))
+                    {
+                        isValidPasscode = true;
+                        Passcode = result.ToString();
+                        return result.ToString();
+                    }
+                }
+            }
+            else
+            {
+                isValidPasscode = false;
+            }
+        }
+        
+        return "Wrong.";
+    }
+
+    private void CreateNewPasscode()
+    {
+        Console.WriteLine("Please enter a 4 digit numerical passcode:");
         string? passcode = Console.ReadLine();
-
-        // The happy path check
+        
         bool blankResponse = String.IsNullOrEmpty(passcode);
         if (!blankResponse)
         {
-            if (passcode.Length == 4)
+            if (passcode!.Length == 4)
             {
                 if (int.TryParse(passcode, out int result))
                 {
                     Passcode = result.ToString();
-                    return result.ToString();
                 }
             }
         }
+    }
+    
+    public string ChangePasscode()
+    {
+        Console.Write("Please enter the current passcode: ");
+        string? currentPasswordAttempt = Console.ReadLine();
 
-        return "Wrong.";
+        if (currentPasswordAttempt == Passcode)
+        {
+            Console.Write("Great, what would you like the new passcode to be? ");
+            CreateNewPasscode();
+        }
+        return "";
     }
 
     public void OpenDoor()
     {
-        if (Closed)
+        if (Closed && Unlocked)
         {
             Open = true;
             Closed = false;
@@ -51,7 +93,7 @@ public class Door
         }
         else
         {
-            Console.WriteLine("Sorry, the door isn't closed so you can't open it.");
+            Console.WriteLine("ERROR: Sorry, you can't open the door right now. It has to be closed and unlocked");
         }
     }
 
@@ -65,20 +107,20 @@ public class Door
         }
         else
         {
-            Console.WriteLine("Can't close a door if it isn't open, Jack.");
+            Console.WriteLine("ERROR: Can't close a door if it isn't open, Jack.");
         }
     }
 
     public void LockDoor()
     {
-        if (Unlocked && Closed)
+        if (Closed && Unlocked)
         {
             Locked = true;
             Console.WriteLine("Congratulations on your newly locked door.");
         }
         else
         {
-            Console.WriteLine("Try closing the door first, bub.");
+            Console.WriteLine("ERROR: The door much be closed and unlocked to lock it.");
         }
     }
 
@@ -97,7 +139,7 @@ public class Door
         }
         else
         {
-            Console.WriteLine("Sorry, can't do that.");
+            Console.WriteLine("ERROR: Sorry, can't do that.");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,29 +13,40 @@ Console.Clear();
 // Create door with user passcode
 Door door = new Door();
 Console.WriteLine("***********************************");
-Console.WriteLine("Nice work. Your new door is closed.");
+Console.WriteLine("Nice work. Your new door is CLOSED and LOCKED.");
 Console.WriteLine($"Initial passcode set to: {door.Passcode}");
 
-// Change door state
-Console.Write("Please feel free to change the door's state -> ");
-string? userInput = Console.ReadLine();
+bool exit = false;
+while (exit == false)
+{
+    // Change door state
+    Console.Write("Please feel free to change the door's state -> ");
+    string? userInput = Console.ReadLine();
 
 // Calls to change state
-switch (userInput)
-{
-    case "open":
-        door.OpenDoor();
-        break;
-    case "close":
-        door.CloseDoor();
-        break;
-    case "lock":
-        door.LockDoor();
-        break;
-    case "unlock":
-        door.UnlockDoor();
-        break;
-    default:
-        Console.WriteLine("Try again.");
-        break;
+    switch (userInput)
+    {
+        case "open":
+            door.OpenDoor();
+            break;
+        case "close":
+            door.CloseDoor();
+            break;
+        case "lock":
+            door.LockDoor();
+            break;
+        case "unlock":
+            door.UnlockDoor();
+            break;
+        case "change passcode":
+            door.ChangePasscode();
+            break;
+        case "exit":
+            Console.WriteLine("K, bye!");
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Try again.");
+            break;
+    }
 }
